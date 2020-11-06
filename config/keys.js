ecba9757545399534e875ff00dab758d3057216e
Andreia <chapman.andreia@gmail.com>
2020-11-06 12:08:50 -0500

    connected front and back end

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://ACC24:mypassword@cluster0.juyoz.mongodb.net/User?retryWrites=true&w=majority",
-    secretOrKey: "secret"
-  };
+  mongoURI: "mongodb+srv://ACC24:mypassword@cluster0.juyoz.mongodb.net/User?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
