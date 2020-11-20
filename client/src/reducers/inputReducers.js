ca4186a8a5f7e6ca2d3f27b8d1e81ef4bfe02d85
Andreia <chapman.andreia@gmail.com>
2020-11-19 20:18:07 -0500

    merging

---
+++ client/src/reducers/inputReducers.js
@@ -0,0 +1,18 @@
+import { GET_USER_INPUT } from "../actions/types";
+
+const initialState = {
+    input: ""
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_USER_INPUT:
+      return {
+          ...state,
+          input: action.payload, ...state.input
+      }
+    default:
+      return state;
+  }
+}
