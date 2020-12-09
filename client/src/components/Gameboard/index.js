f0e06e8bbdba11f0a8c00f9a666cbdf42fbbc691
Eugenia <eugeniasol13@gmail.com>
2020-12-08 21:37:54 -0500

    carousel-fix

--- client/src/components/Gameboard/index.js
+++ client/src/components/Gameboard/index.js
@@ -34,11 +34,13 @@ import "./Gameboard.css";
             >
               <div className="col s12 offset-s8" style={{ marginTop: "20px" }}>
                 <br></br>
-                <h4 style={{ fontWeight: "bold", textAlign: "center" }}>
-                  Hello {user.name.split(" ")[0]}! <Intro></Intro>
+                <h4 style={{ fontWeight: "bold", textAlign: "center", }}>Hello {user.name.split(" ")[0]}!</h4>
+                <h4 style={{ fontWeight: "bold", textAlign: "center", }}>
+                   <Intro></Intro>
                 </h4>
                 <br></br>
-
+                
+                <h4 style={{ fontWeight: "bold", textAlign: "center", }}>
                 <Button
                   type="submit translucent-form-overlay"
                   style={{
@@ -46,13 +48,14 @@ import "./Gameboard.css";
                     borderColor: "yellow",
                     backgroundColor: "rgba(54, 54, 54, 0.8)",
                     borderRadius: "25px",
-                    marginBottom: "1rem",
-                    marginLeft: "450px",
+                    marginBottom:"20px",
                   }}
                   onClick={this.onStartClick}
                 >
                   Start
                 </Button>
+                </h4>
+
               </div>
             </div>
           </div>
